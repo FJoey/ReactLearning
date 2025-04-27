071b11db88ed524005ca8b83e9ee6b39d8b86e87
FunctionJoey <functionjoey@gmail.com>
2025-04-26 18:01:47 -0600

    Click Events

--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -8,7 +8,28 @@ function Button(){
             cursor: "pointer",
     }
 
-    return(<button style={styles}>Click Me</button>);
+    const handleClick = (e) => e.target.textContent = "OUCH!";
+    return(<button onDoubleClick={(e) => handleClick(e)}>Click me</button>);
+
+    /*
+    let count = 0;
+    const handleClick = (name) => {
+        if (count < 3){
+            count++;
+            console.log(`${name} you clicked me ${count} time/s`);
+        } else {
+            console.log(`${name} stop clicking me!`);
+        }
+    }
+    */
+
+    //const handleClick = () => console.log("OUCH!");
+
+    // const handleClick2 = (name) => console.log(`${name} stop clicking me`);
+
+    // return(<button onClick={() => handleClick2("Big Man Blastoise")} style={styles}>Click Me</button>);
+
+    // return(<button onClick={() => handleClick("Big Man Blastoise")} style={styles}>Click Me</button>);
 }
 
 export default Button

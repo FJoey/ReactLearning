53209a8df0395e13d4649017098bb0bccf4d9756
FunctionJoey <functionjoey@gmail.com>
2025-04-30 16:47:28 -0600

    useContext hook

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,10 @@
-import DigitalClock from "./DigitalClock";
+import ComponentA from "./ComponentA";
 
 function App() {
 
     return( 
       <>
-        <DigitalClock />
+        <ComponentA />
       </>
     );
 }
